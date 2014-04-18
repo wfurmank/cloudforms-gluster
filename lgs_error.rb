c430c142ae62f46f820b8f4908c9637bcb311631
wfurmank <wfurmank@redhat.com>
2014-04-18 23:30:29 +0200

    Create lgs_error.rb

---
+++ lgs_error.rb
@@ -0,0 +1,51 @@
+#
+#            Automate Method
+#
+$evm.log("info", "lgs_error Automate Method Started *************************")
+#
+#            Method Code Goes here
+#
+
+
+miq_server = $evm.root['miq_server']
+user = $evm.root['user']
+admin = $evm.vmdb('user').find_by_name("Administrator")
+message = $evm.object['message_body']
+subject = $evm.object['message_subject']
+from = $evm.object['from']
+to = user.email
+to_admin = admin.email
+
+# Formatting email mesage
+body  = "Dear #{user.name},\n"
+body += "<br>\n<br>The following message was genereated per your <i>List Gluster Storage</i> request.\n<br>\n"
+body += "<br>Available free space in Gluster Storage Systems:\n<br>\n"
+body += message
+body += "<br>\n<br>\n<br>-----\n"
+body += "<br>#{miq_server.hostname} CloudForms Management Engine\n<br>\n"
+
+# Logging email body
+$evm.log("info","\n\n==== user email body ====>#{body}<===\n")
+
+
+# If user other than admin, send notification to admin
+unless user.userid == "admin"
+  # Admin email
+  subject_admin = subject + " - User: #{user.name} (#{user.userid})"
+  body_admin  = "Dear #{admin.name},\n"
+  body_admin += "<br>\n<br>The following message was sent to user: #{user.name} (#{user.userid})\n<br>\n"
+  body_admin += body
+
+  # Logging admin email body
+  $evm.log("info","\n\n==== admin email body ====>#{body_admin}<===\n")
+
+  # Sending admin email
+  $evm.execute(:send_email,to_admin,from,subject_admin,body_admin,content_type = nil)
+end
+
+
+#
+#
+#
+$evm.log("info", "lgs_error Automate Method Ended")
+exit MIQ_OK
