802fd1b9d737c88a936b2fdb902e22db3fc5aa7d
wfurmank <wfurmank@redhat.com>
2014-04-24 19:33:10 +0200

    Update dgv_volume.rb

--- dgv_volume.rb
+++ dgv_volume.rb
@@ -6,6 +6,7 @@ $evm.log("info", "dgv_volume Automate Method Started **************************"
 #            Method Code Goes here
 #
 
+@debug = true
 
 userid = $evm.root['user'].userid
 gluster_systems = $evm.object['gluster_systems']
@@ -13,7 +14,7 @@ console = $evm.object['dialog_console']
 cluster = $evm.object['dialog_cluster']
 name = $evm.object['dialog_name']
 
-# Checking if user owns the volume
+# Checking if user owns the volume or is admin
 unless name =~ /^#{userid}_/ or userid == "admin"
   $evm.log("error","User #{userid} is trying to delete volume #{name} (not an owner)")
   exit MIQ_ERROR
@@ -30,10 +31,10 @@ cert = "/tmp/" + console + ".gluster.crt"
 `wget -O #{cert} http://#{console}/ca.crt 2>/dev/null`
 
 # Preparing curl syntax
-curl = "curl -H \"Accept: application/xml\" -H \"Content-Type: application/xml\" -u #{cred} --cacert #{cert} https://#{console}"
+curl = "curl 2> /dev/null -H \"Accept: application/xml\" -H \"Content-Type: application/xml\" -u #{cred} --cacert #{cert} https://#{console}"
 
 # Looking for cluster id by name
-for line in `#{curl}/api/clusters 2> /dev/null`.each_line 
+for line in `#{curl}/api/clusters`.each_line 
   found = false
   clid = line.split('"')[3..3].join if line.include? "<cluster href="
   if line.include? "<name>#{cluster}<"
@@ -48,7 +49,7 @@ unless found
 end
 
 # Looking for volume id by name
-for line in `#{curl}/api/clusters/#{clid}/glustervolumes 2>/dev/null`.each_line
+for line in `#{curl}/api/clusters/#{clid}/glustervolumes`.each_line
   found = false
   volid = line.split('"')[3..3].join if line.include? "<gluster_volume href="
   if line.include? "<name>#{name}<"
@@ -63,17 +64,17 @@ unless found
 end
 
 # Stopping volume
-stop_output = `#{curl}/api/clusters/#{clid}/glustervolumes/#{volid}/stop -d "<action/>" 2>/dev/null`
+stop_output = `#{curl}/api/clusters/#{clid}/glustervolumes/#{volid}/stop -d "<action/>"`
 
 # Deleting volume
-delete_output = `#{curl}/api/clusters/#{clid}/glustervolumes/#{volid} -XDELETE 2>/dev/null`
+delete_output = `#{curl}/api/clusters/#{clid}/glustervolumes/#{volid} -XDELETE`
 
 # Debug
-$evm.log("info","\n\n======= stopping volume =====>>>> #{stop_output}\n")
-$evm.log("info","\n\n======= deleting volume =====>>>> #{delete_output}\n")
+$evm.log("info","\n\n======= stopping volume =====>>>> #{stop_output}\n") if @debug
+$evm.log("info","\n\n======= deleting volume =====>>>> #{delete_output}\n") if @debug
 
 # Checking if volume still exists
-if `#{curl}/api/clusters/#{clid}/glustervolumes/#{volid} 2>/dev/null`.include? volid
+if `#{curl}/api/clusters/#{clid}/glustervolumes/#{volid}`.include? volid
   $evm.log("error","Failed to delete volume: #{name}, id: #{volid}")  
   exit MIQ_ERROR
 end
